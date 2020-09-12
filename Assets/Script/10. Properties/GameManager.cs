c83cca71f6c538c35c12d178789a24d5c44e475b
famg <3d.famg@gmail.com>
2020-09-12 16:17:19 -0300

    unidad 10, propiedades
    como usar get y set

---
+++ Assets/Script/10. Properties/GameManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// public class GameManager : MonoBehaviour
+// {
+// void Start () 
+//     {
+        
+//     }
+// void Update () 
+//     {
+        
+//     }
+// }
+
+// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+
+// 00_Properties
+// // ejemplo de gamemanager, get, set
+// public class GameManager : MonoBehaviour
+// {
+//     private bool isGameOver;
+//     public bool IsGameOver
+//     {
+//         get
+//         {
+//             return isGameOver;
+//         }
+//         set
+//         {
+//             if (value == true)
+//             {
+//                 Debug.Log("oh no! The game is over!");
+//             }
+//             isGameOver = value;
+//         }
+//     }
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         isGameOver = false;
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+//         if (Input.GetKeyDown(KeyCode.Space))
+//         {
+//             IsGameOver = true;
+//         }
+//     }
+// }
+
+// tambien se puede escbrir
+// public int/float/string/bool MyProperty { get; set; }
+
+// que es lo mismo que (para hacerlo privado, se puede hacer lo sgte { get; privateset; })
+
+//     private bool myProperty;
+//     public bool MyProperty
+//     {
+//         get
+//         {
+//             return myProperty;
+//         }
+//         set
+//         {
+//             myProperty = value;
+//         }
+// // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+public class GameManager : MonoBehaviour
+{
+void Start () 
+    {
+        
+    }
+void Update () 
+    {
+        
+    }
+}
