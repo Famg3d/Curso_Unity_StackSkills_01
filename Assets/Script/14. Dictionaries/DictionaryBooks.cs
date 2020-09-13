d3b28dd9edced5cbde0f175bc6b3744f1fa02217
famg <3d.famg@gmail.com>
2020-09-13 03:17:52 -0300

    14. Dictionaries Completado
    como usar dictionary, entender funcionamiento entre una lista elementos con string y int

---
+++ Assets/Script/14. Dictionaries/DictionaryBooks.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DictionaryBooks : MonoBehaviour
+{
+    public Dictionary<int, string> books = new Dictionary<int, string>();
+    private void Start()
+    {
+        books.Add(0, "Logar Book");
+        books.Add(1, "Imperium Book");
+
+        Debug.Log("------------METODO FOREACH 1--------------");
+
+        foreach(KeyValuePair<int, string> book in books)
+        {
+            Debug.Log("Book: " + book.Value);
+        }
+
+            Debug.Log("-----------METODO FOREACH 2---------------");
+
+        foreach(string book in books.Values)
+        {
+            Debug.Log("Book: " + book);
+        }
+            Debug.Log("-----------METODO FOR---------------");
+        for (int i = 0; i < books.Count; i++)
+        {
+            Debug.Log("Book: " + books[i]);
+        }
+
+            Debug.Log("-----------FORMA DIRECTA---------------");
+
+            Debug.Log(books[1]);
+
+
+    }
+
+}
---
+++ Assets/Script/14. Dictionaries/DictionaryPlayerConnections.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerC
+{
+    public string name;
+    public int id;
+
+    public PlayerC(int id)
+    {
+        this.id = id;
+    }
+}
+public class DictionaryPlayerConnections : MonoBehaviour
+{
+    public Dictionary<int, PlayerC> playerDictionary = new Dictionary<int, PlayerC>();
+    PlayerC p4;
+    private void Start()
+    {
+        PlayerC p1 = new PlayerC(1);
+        p1.name = "jimmy";
+        PlayerC p2 = new PlayerC(200);
+        p2.name = "kyle";
+        PlayerC p3 = new PlayerC(9);
+        p3.name = "yin";
+        p4 = new PlayerC(500);
+        p4.name = "juan";
+
+        playerDictionary.Add(p1.id, p1);
+        playerDictionary.Add(p2.id, p2);
+        playerDictionary.Add(p3.id, p3);
+        playerDictionary.Add(p4.id, p4);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            var player = playerDictionary[200];
+            Debug.Log("Player name: " + player.name);
+
+            var player1 = playerDictionary[p4.id];
+            Debug.Log("Player name: " + player1.name);
+        }
+    }
+}
