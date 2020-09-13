b91a731a446625d95be578fb74474b2358bcbd47
famg <3d.famg@gmail.com>
2020-09-13 00:48:59 -0300

    13. Enums completo
    como setear basicamente la dificultad del juego y los estados del enemigo con listas

---
+++ Assets/Script/13. Enums/EnemyAI.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+   public enum EnemyState
+   {
+        Patroling,
+        Attacking,
+        Chasing,
+        Death
+   } 
+        public EnemyState currentState;
+        void Start()
+    {
+        currentState = EnemyState.Patroling;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (currentState)
+        {
+            case EnemyState.Patroling:
+            Debug.Log("Patrolling");
+            break;
+            case EnemyState.Attacking:
+            Debug.Log("Attacking");
+            break;
+            case EnemyState.Chasing:
+            Debug.Log("Chasing");
+            break;
+            case EnemyState.Death:
+            Debug.Log("Death");
+            break;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            currentState = EnemyState.Attacking;
+        }
+    }
+}
